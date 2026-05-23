fb3a29f6e02b2d9db597598094277b482f5f69c7
isaiah1jacob <isaiah1jacob@gmail.com>
2026-05-23 04:09:53 -0700

    added hello tag

--- components/page/Episodes.tsx
+++ components/page/Episodes.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View } from 'react-native'
 import PageHead from '../PageHead'
 
 const Episodes = () => {
   return (
     <View className='h-full w-full -mt-10'>
       <PageHead title='Episodes' has_menu has_priv={false}/>
+      <Text>Hello</Text>
     </View>
   )
 }
